e52d8c9d43fc5cbd033e41f026577898d873a0dd
changsungbeom <iwsl1234@naver.com>
2020-09-11 16:49:03 +0900

    쿵쿵따 코드

--- FirstFrontPractice.js
+++ FirstFrontPractice.js
@@ -1,4 +1,4 @@
-var object = {
+/*var object = {
     id:"myid",
     name : "장성범"
 }
@@ -11,4 +11,21 @@ var elm1=document.body;
 var elm2=document.createElement("div");
 
 elm2.textContent="성범이";
-document.body.append(elm2);
+document.body.append(elm2);*/
+
+var originWord='장성범';
+while(true){
+    var inputString = prompt('문자열을 입력하세요', '기본 값 문자열'); 
+    console.log(inputString[0]);
+    console.log(originWord[originWord.length-1]);
+    if(originWord[originWord.length-1]==inputString[0]){
+        alert('쿵쿵따 성공');
+        originWord=inputString;
+        alert('다음문자는'+originWord);
+    }
+
+    else{
+        alert('쿵쿵따 실패');
+
+    }
+}
