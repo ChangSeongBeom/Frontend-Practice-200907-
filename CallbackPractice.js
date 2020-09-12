3f2961bd8e7e162018b8a52383880c232f190da3
changsungbeom <iwsl1234@naver.com>
2020-09-12 20:30:13 +0900

    간단한 콜백 끝말잇기

--- CallbackPractice.js
+++ CallbackPractice.js
@@ -2,10 +2,18 @@ var buttonTest=document.createElement('button');
 buttonTest.textContent="CLICK";
 document.body.append(buttonTest);
  
+var originWord='장성범';
 
 buttonTest.addEventListener('click', function callbackFunction(){
-    var test=document.getElementById('nameValue').value;
-    alert(test);
+    var word=document.getElementById('nameValue').value;
+
+    if(originWord[originWord.length-1]==word[0]){
+        alert('정답이에요');
+        originWord=word;
+    }
+    else{
+        alert('틀렸어요');
+    }
 });
 
 
